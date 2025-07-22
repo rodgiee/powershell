fbd0a1d2749e56242efd2c02e25377b4c1508717
rodgiee <rebecarodge@gmail.com>
2025-07-22 14:14:47 -0400

    fix: incorrect suffix and cleaned up [int] cast code

--- availability_dates.ps1
+++ availability_dates.ps1
@@ -3,7 +3,7 @@ function Get-DaySuffix
 	Param(
 		[System.DateTime]$date_input
 	)
-	switch ([int]$date_input.DayOfMonth)
+	switch ($date_input.Day)
 	{
 		{$_ -in 1,21,31} {$suffix = 'st'}
 		{$_ -in 2,22} {$suffix = 'nd'}
@@ -13,17 +13,17 @@ function Get-DaySuffix
 
 return $suffix
 }
-$next_monday = (Get-Date).AddDays(8 - [int](Get-Date).DayOfWeek)  # [datetime] Get the date of the next monday 
+$next_monday = (Get-Date).AddDays(8 - (Get-Date).DayOfWeek)  # [datetime] Get the date of the next monday 
 $clipboard = "" # [string] initialize clipboard contents, to be filled in with dates
 
 for($i = 0; $i -lt 5 ; $i++){ # Add to clipboard each day for next week
 	$current_date = $next_monday.AddDays($i)  
 	$current_suffix = Get-DaySuffix -date_input ($current_date) # [string] obtain suffix for specific date (ex. th, rd, etc)
 
-	if(([int]$current_date.DayOfWeek -eq 2) -or ([int]$current_date.DayOfWeek -eq 4)){ # Check if Tuesday or Thursday (times changed due to morning meeting)
-		$clipboard += "$($current_date | Get-Date -Format "dddd, MMMM, dd")$current_suffix, 11am to 5pm`n" # Availability times for Tuesday and Thursday
+	if(($current_date.DayOfWeek -eq 2) -or ($current_date.DayOfWeek -eq 4)){ # Check if Tuesday or Thursday (times changed due to morning meeting)
+		$clipboard += "$($current_date | Get-Date -Format "dddd, MMMM d")$current_suffix, 11am to 5pm`n" # Availability times for Tuesday and Thursday
 	}else{
-	$clipboard += "$($current_date | Get-Date -Format "dddd, MMMM, dd")$current_suffix, 10am to 5pm`n" # Availability times for any other day
+	$clipboard += "$($current_date | Get-Date -Format "dddd, MMMM d")$current_suffix, 10am to 5pm`n" # Availability times for any other day
 	}
 }
 Set-Clipboard $clipboard
